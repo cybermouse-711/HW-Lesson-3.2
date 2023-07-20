091186b2b3299f3645c186fbf0015105b1c5b36a
ChauveSouris <desseobscurite@gmail.com>
2023-07-20 21:35:56 +0300

    Created Extension

--- HW Lesson 3.2/ViewController.swift
+++ HW Lesson 3.2/ViewController.swift
@@ -15,9 +15,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        fetchSpaceX()
     }
-
-
 }
 
+extension ViewController {
+    
+    private func fetchSpaceX() {
+        guard let url = URL(string: Link.spaceXURL.rawValue) else { return }
+        
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let data else {
+                print(error?.localizedDescription ?? "Not error")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let spaceX = try decoder.decode(SpaceX.self, from: data)
+                print(spaceX)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+}
