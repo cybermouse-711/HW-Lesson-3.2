6d95de35d51d420d4b3f4db52faaeecd7542e72b
ChauveSouris <desseobscurite@gmail.com>
2023-07-23 19:47:25 +0300

    Incorected Model

--- HW Lesson 3.2/Model.swift
+++ HW Lesson 3.2/Model.swift
@@ -8,36 +8,41 @@
 import Foundation
 
 struct SpaceX: Decodable {
-    let links: Links?
+    let links: Links
     let static_fire_date_utc: String?
     let static_fire_date_unix: String?
-    let net: Bool?
+    let net: Bool
     let window: String?
-    let rocket: String?
-    let success: Bool?
+    let rocket: String
+    let success: Bool
     let failures: [Int]?
     let details: String?
+    let crew: Crew
+    let flight_number: Int
+    let name: String
+    let date_utc: String
+    let date_local: String
 }
 
 struct Links: Decodable {
-    let patch: Patch?
-    let reddit: Reddit?
+    let patch: Patch
+    let reddit: Reddit
     let flickr: Flickr?
     let presskit: String?
-    let webcast: String?
-    let youtube_id: String?
+    let webcast: String
+    let youtube_id: String
     let article: String?
-    let wikipedia: String?
+    let wikipedia: String
 }
 
 struct Patch: Decodable {
-    let small: String?
-    let large: String?
+    let small: String
+    let large: String
 }
 
 struct Reddit: Decodable {
     let campaign: String?
-    let launch: String?
+    let launch: String
     let media: String?
     let recovery: String?
 }
@@ -45,4 +50,15 @@ struct Reddit: Decodable {
 struct Flickr: Decodable {
     let small: [Int]?
     let original: [Int]?
+}
+
+struct Crew: Decodable {
+    let 0: Comand
+    let 1: Comand
+    let 2: Comand
+    let 3: Comand
+}
+
+struct Comand: Decodable {
+    let role: String
 }
