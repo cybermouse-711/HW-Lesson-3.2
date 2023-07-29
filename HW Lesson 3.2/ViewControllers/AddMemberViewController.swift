ce58ce2d2d7de076d033c3a9277eedf66122f46a
ChauveSouris <desseobscurite@gmail.com>
2023-07-30 01:05:42 +0300

    Created VC for Added a Member

---
+++ HW Lesson 3.2/ViewControllers/AddMemberViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddMemberViewController.swift
+//  HW Lesson 3.2
+//
+//  Created by Елизавета Медведева on 30.07.2023.
+//
+
+import UIKit
+
+class AddMemberViewController: UIViewController {
+
+    @IBOutlet var idTextField: UITextField!
+    @IBOutlet var memberTextField: UITextField!
+    @IBOutlet var saveButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.tintColor = UIColor.gray
+
+    }
+
+
+
+}
